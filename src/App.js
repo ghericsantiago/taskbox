910502d35a0ab8f1f79e03ddce1bfb51eccaf8ae
Gheric Santiago <gheric.santiago@directwithhotels.com>
2019-07-31 11:45:46 +0800

    Integrate Redux

--- src/App.js
+++ src/App.js
@@ -4,18 +4,9 @@ import TaskList from './components/TaskList';
 
 function App() {
 
-  const defaultTasks = [
-    { id: '1', title: 'Task 1', state: 'TASK_INBOX' },
-    { id: '2', title: 'Task 2', state: 'TASK_INBOX' },
-    { id: '3', title: 'Task 3', state: 'TASK_INBOX' },
-    { id: '4', title: 'Task 4', state: 'TASK_INBOX' },
-    { id: '5', title: 'Task 5', state: 'TASK_INBOX' },
-    { id: '6', title: 'Task 6', state: 'TASK_INBOX' },
-  ];  
-
   return (
     <div className="App">
-      <TaskList tasks={defaultTasks} onPinTask={()=>{}} onArchiveTask={()=>{}} />
+      <TaskList/>
     </div>
   );
 }
