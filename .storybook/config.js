a499d2c21e8ed696bb66668fbade7355fa0ac339
Gheric Santiago <gheric.santiago@directwithhotels.com>
2019-07-31 10:41:49 +0800

    Storybook Tutorial Initial Commit

---
+++ .storybook/config.js
@@ -0,0 +1,23 @@
+import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+
+// import compiled css to be used in storybook
+import '../src/index.css';
+
+// By default storybook will look for src/stories folder
+/* 
+function loadStories() {
+    require('../src/stories'); 
+}
+*/
+
+// Look for a file with extension name '.stories.js' (This will replace the default behavior above)
+// const req = require.context('../src', true, /\.stories.js$/);
+
+// Replace require.context to requireContext to run it properly in jest
+const req = requireContext('../src', true, /\.stories.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TaskList from './components/TaskList';
+
 
 function App() {
+
+  const defaultTasks = [
+    { id: '1', title: 'Task 1', state: 'TASK_INBOX' },
+    { id: '2', title: 'Task 2', state: 'TASK_INBOX' },
+    { id: '3', title: 'Task 3', state: 'TASK_INBOX' },
+    { id: '4', title: 'Task 4', state: 'TASK_INBOX' },
+    { id: '5', title: 'Task 5', state: 'TASK_INBOX' },
+    { id: '6', title: 'Task 6', state: 'TASK_INBOX' },
+  ];  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskList tasks={defaultTasks} onPinTask={()=>{}} onArchiveTask={()=>{}} />
     </div>
   );
 }
